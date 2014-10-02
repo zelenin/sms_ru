04144ca936bc55cb4f62c004cf5a70ddd6cfe33f
Aleksandr Zelenin <aleksandr@zelenin.me>
2014-10-02 18:50:52 +0400

    big refactoring

--- Smsru.php
+++ Smsru.php
@@ -2,30 +2,39 @@
 
 namespace Zelenin;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+
 class Smsru
 {
-    const VERSION = '2.0.0';
-    const HOST = 'http://sms.ru/';
-    const SEND = 'sms/send?';
-    const STATUS = 'sms/status?';
-    const COST = 'sms/cost?';
-    const BALANCE = 'my/balance?';
-    const LIMIT = 'my/limit?';
-    const SENDERS = 'my/senders?';
-    const GET_TOKEN = 'auth/get_token';
-    const CHECK = 'auth/check?';
-    const ADD = 'stoplist/add?';
-    const DEL = 'stoplist/del?';
-    const GET = 'stoplist/get?';
-    const UCS = 'sms/ucs?';
+    /** @var Client */
+    private $client = null;
     private $apiId;
     private $login;
     private $password;
-    private $params;
+    private $authParams = [];
     private $token;
     private $sha512;
-    private $response_code = array(
-        'send' => array(
+
+    const VERSION = '2.0.0';
+    const HOST = 'sms.ru';
+    const SEND = 'sms/send';
+    const STATUS = 'sms/status';
+    const COST = 'sms/cost';
+    const BALANCE = 'my/balance';
+    const LIMIT = 'my/limit';
+    const SENDERS = 'my/senders';
+    const GET_TOKEN = 'auth/get_token';
+    const CHECK = 'auth/check';
+    const ADD = 'stoplist/add';
+    const DEL = 'stoplist/del';
+    const GET = 'stoplist/get';
+    const UCS = 'sms/ucs';
+
+    const MAX_TIME = 604800; //7 * 60 * 60 * 24
+
+    private $response_code = [
+        'send' => [
             '100' => 'Сообщение принято к отправке. На следующих строчках вы найдете идентификаторы отправленных сообщений в том же порядке, в котором вы указали номера, на которых совершалась отправка.',
             '200' => 'Неправильный api_id.',
             '201' => 'Не хватает средств на лицевом счету.',
@@ -43,8 +52,8 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'status' => array(
+        ],
+        'status' => [
             '-1' => 'Сообщение не найдено.',
             '100' => 'Сообщение находится в нашей очереди.',
             '101' => 'Сообщение передается оператору.',
@@ -62,8 +71,8 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'cost' => array(
+        ],
+        'cost' => [
             '100' => 'Запрос выполнен. На второй строчке будет указана стоимость сообщения. На третьей строчке будет указана его длина.',
             '200' => 'Неправильный api_id.',
             '202' => 'Неправильно указан получатель.',
@@ -74,8 +83,8 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'balance' => array(
+        ],
+        'balance' => [
             '100' => 'Запрос выполнен. На второй строчке вы найдете ваше текущее состояние баланса.',
             '200' => 'Неправильный api_id.',
             '210' => 'Используется GET, где необходимо использовать POST.',
@@ -84,8 +93,8 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'limit' => array(
+        ],
+        'limit' => [
             '100' => 'Запрос выполнен. На второй строчке вы найдете ваше текущее дневное ограничение. На третьей строчке количество сообщений, отправленных вами в текущий день.',
             '200' => 'Неправильный api_id.',
             '210' => 'Используется GET, где необходимо использовать POST.',
@@ -94,8 +103,8 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'senders' => array(
+        ],
+        'senders' => [
             '100' => 'Запрос выполнен. На второй и последующих строчках вы найдете ваших одобренных отправителей, которые можно использовать в параметре &from= метода sms/send.',
             '200' => 'Неправильный api_id.',
             '210' => 'Используется GET, где необходимо использовать POST.',
@@ -104,44 +113,40 @@ class Smsru
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'check' => array(
+        ],
+        'check' => [
             '100' => 'ОК, номер телефона и пароль совпадают.',
             '300' => 'Неправильный token (возможно истек срок действия, либо ваш IP изменился).',
             '301' => 'Неправильный пароль, либо пользователь не найден.',
             '302' => 'Пользователь авторизован, но аккаунт не подтвержден (пользователь не ввел код, присланный в регистрационной смс).'
-        ),
-        'add' => array(
+        ],
+        'add' => [
             '100' => 'Номер добавлен в стоплист.',
             '202' => 'Номер телефона в неправильном формате.'
-        ),
-        'del' => array(
+        ],
+        'del' => [
             '100' => 'Номер удален из стоплиста.',
             '202' => 'Номер телефона в неправильном формате.'
-        ),
-        'get' => array(
+        ],
+        'get' => [
             '100' => 'Запрос обработан. На последующих строчках будут идти номера телефонов, указанных в стоплисте в формате номер;примечание.'
-        )
-    );
+        ]
+    ];
 
-    public function  __construct($api_id = null, $login = null, $pwd = null)
+    public function  __construct($apiId = null, $login = null, $password = null)
     {
-        $this->apiId = $api_id;
+        $this->apiId = $apiId;
         $this->login = $login;
-        $this->password = $pwd;
-
-        $this->params = $this->getAuthParams();
+        $this->password = $password;
+        $this->authParams = $this->getAuthParams();
     }
 
-    public function sms_send($to, $text, $from = null, $time = null, $translit = false, $test = false, $partner_id = null) {
-        $messages = array(array($to, $text));
-        return $this->multi_sms_send($messages, $from, $time, $translit, $test, $partner_id);
+    public function smsSend($to, $text, $from = null, $time = null, $translit = false, $test = false, $partner_id = null) {
+        $messages = [[$to, $text]];
+        return $this->multiSmsSend($messages, $from, $time, $translit, $test, $partner_id);
     }
 
-    public function multi_sms_send($messages, $from = null, $time = null, $translit = false, $test = false, $partner_id = null) {
-        $url = self::HOST . self::SEND;
-        $params = $this->params;
-
+    public function multiSmsSend($messages, $from = null, $time = null, $translit = false, $test = false, $partner_id = null) {
         foreach ($messages as $message) {
             $params['multi'][$message[0]] = $message[1];
         }
@@ -150,7 +155,7 @@ class Smsru
             $params['from'] = $from;
         }
 
-        if ($time && $time < (time() + 7 * 60 * 60 * 24)) {
+        if ($time && $time < (time() + static::MAX_TIME)) {
             $params['time'] = $time;
         }
 
@@ -166,10 +171,10 @@ class Smsru
             $params['partner_id'] = $partner_id;
         }
 
-        $result = $this->curl($url, http_build_query($params, '', '&'));
+        $result = $this->method(static::SEND, $params);
         $result = explode("\n", $result);
 
-        $response = array();
+        $response = [];
         $response['code'] = array_shift($result);
         $response['description'] = $this->getAnswer('send', $response['code']);
 
@@ -186,96 +191,84 @@ class Smsru
         return $response;
     }
 
-    public function sms_mail($to, $text, $from = null)
+    public function smsMail($to, $text, $from = null)
     {
-        $mail = $this->apiId . '@' . self::HOST;
-        $subject = isset($from) ? $to . ' from:' . $from : $to;
+        $mail = $this->apiId . '@' . static::HOST;
+        $subject = $from ? $to . ' from:' . $from : $to;
         $headers = 'Content-Type: text/html; charset=UTF-8';
         return mail($mail, $subject, $text, $headers);
     }
 
-    public function sms_status($id)
+    public function smsStatus($id)
     {
-        $url = self::HOST . self::STATUS;
-        $params = $this->params;
         $params['id'] = $id;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::STATUS, $params);
 
-        $response = array();
+        $response = [];
         $response['code'] = $result;
         $response['description'] = $this->getAnswer('status', $response['code']);
         return $response;
     }
 
-    public function sms_cost($to, $text)
+    public function smsCost($to, $text)
     {
-        $url = self::HOST . self::COST;
-        $params = $this->params;
         $params['to'] = $to;
         $params['text'] = $text;
 
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::COST, $params);
         $result = explode("\n", $result);
 
-        return array(
+        return [
             'code' => $result[0],
             'description' => $this->getAnswer('cost', $result[0]),
             'price' => $result[1],
             'number' => $result[2]
-        );
+        ];
     }
 
-    public function my_balance()
+    public function myBalance()
     {
-        $url = self::HOST . self::BALANCE;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::BALANCE);
         $result = explode("\n", $result);
-        return array(
+        return [
             'code' => $result[0],
             'description' => $this->getAnswer('balance', $result[0]),
             'balance' => $result[1]
-        );
+        ];
     }
 
     public function my_limit()
     {
-        $url = self::HOST . self::LIMIT;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::LIMIT);
         $result = explode("\n", $result);
-        return array(
+        return [
             'code' => $result[0],
             'description' => $this->getAnswer('limit', $result[0]),
             'total' => $result[1],
             'current' => $result[2]
-        );
+        ];
     }
 
     public function my_senders()
     {
-        $url = self::HOST . self::SENDERS;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::SENDERS);
         $result = explode("\n", rtrim($result));
 
-        $response = array(
+        $response = [
             'code' => $result[0],
             'description' => $this->getAnswer('senders', $result[0]),
             'senders' => $result
-        );
+        ];
         unset($response['senders'][0]);
         $response['senders'] = array_values($response['senders']);
         return $response;
     }
 
-    public function auth_check()
+    public function authCheck()
     {
-        $url = self::HOST . self::CHECK;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
-
-        $response = array();
+        /* todo */
+        $result = $this->method(static::CHECK, $this->authParams);
+        $response = [];
         $response['code'] = $result;
         $response['description'] = $this->getAnswer('check', $response['code']);
         return $response;
@@ -283,13 +276,11 @@ class Smsru
 
     public function stoplist_add($stoplist_phone, $stoplist_text)
     {
-        $url = self::HOST . self::ADD;
-        $params = $this->params;
         $params['stoplist_phone'] = $stoplist_phone;
         $params['stoplist_text'] = $stoplist_text;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::ADD, $params);
 
-        $response = array();
+        $response = [];
         $response['code'] = $result;
         $response['description'] = $this->getAnswer('add', $response['code']);
         return $response;
@@ -297,30 +288,27 @@ class Smsru
 
     public function stoplist_del($stoplist_phone)
     {
-        $url = self::HOST . self::DEL;
-        $params = $this->params;
         $params['stoplist_phone'] = $stoplist_phone;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::DEL, $params);
 
-        $response = array();
+        $response = [];
         $response['code'] = $result;
         $response['description'] = $this->getAnswer('del', $response['code']);
         return $response;
     }
 
-    public function stoplist_get()
+    public function stoplistGet()
     {
-        $url = self::HOST . self::GET;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
+        $result = $this->method(static::GET);
 
         $result = explode("\n", rtrim($result));
-        $response = array(
+        $response = [
             'code' => $result[0],
             'description' => $this->getAnswer('get', $result[0]),
             'stoplist' => $result
-        );
+        ];
         $count = count($response['stoplist']);
+        $stoplist=[];
         for ($i = 1; $i < $count; $i++) {
             $result = explode(';', $response['stoplist'][$i]);
             $stoplist[$i - 1]['number'] = $result[0];
@@ -330,12 +318,9 @@ class Smsru
         return $response;
     }
 
-    public function sms_ucs()
+    public function smsUcs()
     {
-        $url = self::HOST . self::UCS;
-        $params = $this->params;
-        $result = $this->curl($url, $params);
-        return $result;
+        return  $this->method(static::UCS);
     }
 
     private function getAuthParams()
@@ -355,8 +340,7 @@ class Smsru
 
     private function authGetToken()
     {
-        $url = self::HOST . self::GET_TOKEN;
-        return $this->curl($url);
+        return $this->method(static::GET_TOKEN);
     }
 
     private function getSha512()
@@ -373,18 +357,22 @@ class Smsru
             : null;
     }
 
-    private function curl($url, $params = array())
+    public function method($name,$params = [])
     {
-        $ch = curl_init($url);
-        $options = array(
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_POSTFIELDS => $params
-        );
-        curl_setopt_array($ch, $options);
-        $result = curl_exec($ch);
-        curl_close($ch);
+        return $this->request('http://'.static::HOST . '/' .$name,array_merge($params,$this->authParams));
+    }
 
-        return $result;
+    private function request($url, $params = [])
+    {
+        if (!$this->client) {
+            $this->client = new Client();
+        }
+        /** @var Response $response */
+        $response = $this->client->post($url, ['body' => $params]);
+        if ($response->getStatusCode()==200) {
+            return $response->getBody();
+        } else {
+            throw new \Exception('Sms.ru problem. Status code is ' . $response->getStatusCode(),$response->getStatusCode());
+        }
     }
 }
